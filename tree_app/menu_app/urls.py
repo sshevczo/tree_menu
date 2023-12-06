cb91c0fa5c57bf68f3c9b268ee151e7908a6b950
Sergei_Shevtsov <sshevczo1993@gmail.com>
2023-12-06 14:20:24 +0200

    create views,templates,urls,admin,tags_custom

--- tree_app/menu_app/urls.py
+++ tree_app/menu_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import index, draw_menu
 
 urlpatterns = [
 
-    # path('',),
+    path('', index, name='menu_main'),
+    path('<path:path>/', draw_menu, name='draw_menu'),
 ]
--- tree_app/menu_app/views.py
+++ tree_app/menu_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Menu
 
-# Create your views here.
+
+def index(request):
+    context = {'menus': Menu.objects.all()}
+    return render(request, 'menu_app/index.html', context=context)
+
+
+def draw_menu(request, path):
+    splitted_path = path.split('/')
+    assert len(splitted_path) > 0, ('= Draw_menu function failed =')
+    print(splitted_path)
+    return render(
+        request, 'app/index.html', {'menu_name': splitted_path[0], 'menu_item': splitted_path[-1]})
